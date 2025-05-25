5615e7061b2a245a9fb3f181e857e47f66c8ae15
nicolas <nicolas.nkruge@gmaila.com>
2025-05-25 09:13:57 -0300

    :fire:

--- lua/init.lua
+++ lua/init.lua
@@ -1,10 +1,7 @@
--- ~/.config/nvim/lua/plugins/specs/myplugin.lua
-return {
-	lazy = true,
-	cmd = { "SayHello" }, -- Only loads on :SayHello
-	config = function()
-		vim.api.nvim_create_user_command("SayHello", function()
-			require("myplugin").say_hello()
-		end, {})
-	end,
-}
+local M = {}
+
+function M.hello()
+	print("Olá do meu plugin!")
+end
+
+return M
