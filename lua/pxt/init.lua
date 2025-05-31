e61a82cb25cec6a98abf521254fddfb684be7841
nicolas <nicolas.nkruge@gmaila.com>
2025-05-31 09:20:44 -0300

    :fire:

--- lua/pxt/init.lua
+++ lua/pxt/init.lua
@@ -2,6 +2,7 @@ local pxt = {}
 
 function pxt.mono_font()
 	print("change font")
+	vim.api.nvim_set_hl(0, "Normal", { bg = "#1e1e2e" })
 	vim.api.nvim_create_autocmd("BufEnter", {
 		pattern = "*.pxt",
 		callback = function()
