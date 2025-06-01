7eb0adcd41afcc004da93609e4499fc4606efb30
nicolas <nicolas.nkruge@gmaila.com>
2025-06-01 18:16:51 -0300

    :fire:

--- lua/pxt/init.lua
+++ lua/pxt/init.lua
@@ -3,13 +3,12 @@ local pxt = {}
 function pxt.mono_font()
 	print("change font 💿")
 	local aroba = "@"
-	vim.api.nvim_set_hl(0, "Normal", { bg = "#FFFFFF" })
 	vim.api.nvim_set_hl(0, aroba, { bg = "#ff0000", fg = "#ffffff" })
 	vim.fn.matchadd(aroba, aroba)
 	vim.api.nvim_create_autocmd("BufEnter", {
 		pattern = "*.pxt",
 		callback = function()
-			vim.o.guifont = "FiraCode Nerd Font:h14"
+			print("open pxt 🆗")
 		end,
 	})
 end
